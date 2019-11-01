a38330e81563747b95ab01e2675fdc91300a295c
Rebecca Barrett <barrett4467@gmail.com>
2019-10-31 22:13:53 -0600

    Added connection

--- server.js
+++ server.js
@@ -26,7 +26,12 @@ var exphbs = require("express-handlebars");
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-mongoose.connect("mongodb://localhost/recipes", { useNewUrlParser: true});
+// mongoose.connect("mongodb://localhost/recipes", { useNewUrlParser: true});
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/recipes";
+
+mongoose.connect(MONGODB_URI);
+
+// heroku_4gjc89v7
 
 //do a create route to scrape
 app.get("/scrape", function(req, res){
